47003865d37d9675a26549c2d4aa58232d937ef9
Андрей Куктенко <andriy.kuktenko@gmail.com>
2018-11-04 18:14:26 +0200

    cabinet page

--- views/main.tpl.php
+++ views/main.tpl.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php echo $pageData['title']; ?></title>
 	<link rel="stylesheet" href="/css/bootstrap.min.css">
-	<link rel="stylesheet" href="/css/fontawesome.min.css">
+	<link rel="stylesheet" href="/css/font-awesome.min.css">
 	<link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
